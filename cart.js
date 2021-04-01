e798f489614cfd69fad5515d229434c8a3e59645
Noni Shelton <nonishelton@nonis-mbp.wifi.local.cmu.edu>
2021-04-01 16:00:13 -0400

    working price

--- cart.js
+++ cart.js
@@ -10,5 +10,18 @@ for (var i = 0; i < removeCartItemButtons.length; i++) {
 }
 function updateCartTotal(){
 	var cartItemContainer = document.getElementsByClassName("cart-items")[0];
-	cartItemContainer.getElementsByClassName("cart-items")
+	var cartRows = cartItemContainer.getElementsByClassName("first-cart-row");
+	var total = 0;
+	for (var i = 0; i < cartRows.length; i++) {
+		var cartRow = cartRows[i];
+		var priceElement = cartRow.getElementsByClassName("cart-price")[0];
+		console.log(typeof priceElement)
+		var quantityElement = cartRow.getElementsByClassName("cart-quantity-input")[0];
+		var price = parseFloat(priceElement.innerText.replace("$",""));
+		console.log(price)
+		var quantity = quantityElement.value;
+		total = total + (price * quantity);
+		console.log(total)
+	}
+	//document.getElementsByClassName("cart-total-price")[0].innerText = total;
 }
